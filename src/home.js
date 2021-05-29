7855beb901b27a07e04948f4c56b90b1a0393300
Matthew Smart <matthewsmart13@gmail.com>
2021-05-29 00:05:09 -0500

    made home tab

---
+++ src/home.js
@@ -0,0 +1,42 @@
+function createHome() {
+    const home = document.createElement('div');
+    home.classList.add('home-container');
+
+    home.appendChild(createHero());
+    home.appendChild(createHeroImg());
+    return home;
+}
+
+function createHero() {
+    const hero = document.createElement('div');
+    hero.classList.add('hero');
+
+    const heroL = document.createElement('h1');
+    heroL.textContent = 'Pushed to our Knees so let us pray';
+
+    const heroP = document.createElement('p');
+    heroP.textContent = 'The best straight edge vegan resturant in all of CA';
+
+    hero.appendChild(heroL);
+    hero.appendChild(heroP);
+
+    return hero;
+}
+
+function createHeroImg() {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    const punkImg = document.createElement('img');
+    punkImg.src = 'img/straight-edge.jpg';
+    punkImg.alt = 'fist with X for the straight edge movement';
+
+    return imgContainer;
+}
+
+function loadHome() {
+    const main = document.getElementById('content');
+    main.textContent = ''
+    main.appendChild(createHome());
+}
+
+export default loadHome;
