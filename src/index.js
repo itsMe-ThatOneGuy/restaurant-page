2a3ed4c683f0241b6286d3aacdb9399351d68d14
Matthew Smart <matthewsmart13@gmail.com>
2021-08-13 01:33:59 -0500

    created base function for switching tabs and created an event listener for tab selection

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import loadPage from "./mainpage.js"
 import loadHome from "./home.js"
 
 
+
 loadPage();
-currentTab()
 
-function currentTab() {
-    const tabs = document.querySelectorAll(".nav-itm");
+const tabs = document.querySelectorAll(".nav-itm");
 
-    tabs.forEach((tab) => {
-        tab.addEventListener('click',  (e) => {
-            if (e.target.classList.contains("active")) return;
-            console.log(tab.classList);
-        });
+tabs.forEach((tab) => {
+    tab.addEventListener('click', (e) => {
+        //console.log(e.target.className);
+        currentTab(e.target);
     });
-}
+});
 
+function currentTab(tab) {
+    //console.log(tab.className);
+    if(tab.classList.contains('home')) {
+        loadHome();
+    } else {
+        console.log(tab.className);
+    }
+}
