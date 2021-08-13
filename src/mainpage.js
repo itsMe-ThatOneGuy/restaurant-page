81f3a8785e01b7220830fdaf2a7d4fbba8c79cc5
Matthew Smart <matthewsmart13@gmail.com>
2021-08-13 01:32:47 -0500

    added tab name classes to the different tabs

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,3 +1,5 @@
+import loadHome from "./home.js"
+
 function createHeader() {
     const hearder = document.createElement('header');
     hearder.appendChild(createNav());
@@ -12,15 +14,15 @@ function createNav() {
     const ul = document.createElement('ul');
 
     const homeTab = document.createElement('li');
-    homeTab.classList.add('nav-itm');
+    homeTab.classList.add('nav-itm', 'home');
     homeTab.textContent = "Home";
 
     const menuTab = document.createElement('li');
-    menuTab.classList.add('nav-itm');
+    menuTab.classList.add('nav-itm', 'menu');
     menuTab.textContent = "menu";
 
     const contactTab = document.createElement('li');
-    contactTab.classList.add('nav-itm');
+    contactTab.classList.add('nav-itm', 'contact');
     contactTab.textContent = "Contact";
 
     
@@ -36,6 +38,7 @@ function createNav() {
 
 function createMain() {
     const main = document.createElement('main');
+    main.setAttribute('id', 'main');
     return main;
 }
 
@@ -54,6 +57,8 @@ function loadPage() {
     content.appendChild(createHeader());
     content.appendChild(createMain());
     content.appendChild(createFooter());
+    console.log('loaded');
+    loadHome();
 }
 
 export default loadPage;
